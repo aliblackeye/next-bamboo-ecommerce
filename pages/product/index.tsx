4aee03d263307d2713fbad42ed4712078999044e
baython <ali.blackeye@hotmail.com>
2022-10-21 15:31:21 +0300

    Product page's responsive design is finished.

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -133,16 +133,16 @@ const Product: NextPage = () => {
         <title>Bamboo | Product Page</title>
       </Head>
 
-      <div className="container py-[12.5rem] px-5">
-        <div className="flex">
+      <div className="container py-[12.5rem] mx-auto px-5">
+        <div className="flex flex-col lg:flex-row gap-5 justify-center">
           <Swiper
             cssMode={true}
             navigation={true}
             mousewheel={true}
             modules={[Navigation, Mousewheel]}
-            className="product-swiper !h-[50rem] !w-[38.125rem]">
-            <SwiperSlide>
-              <div className="h-[50rem] w-[38.125rem]">
+            className="product-swiper w-[21.875rem] h-[28.6563rem] sm:h-[50rem] sm:w-[38.125rem]"> 
+            <SwiperSlide> 
+              <div className="w-[21.875rem] h-[28.6563rem] sm:h-[50rem] sm:w-[38.125rem]">   
                 <img
                   src="/images/p1.png"
                   className="w-full h-full"
@@ -151,7 +151,7 @@ const Product: NextPage = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="h-[50rem] w-[38.125rem]">
+              <div className="w-[21.875rem] h-[28.6563rem] sm:h-[50rem] sm:w-[38.125rem]">
                 <img
                   src="/images/p3.png"
                   className="w-full h-full"
@@ -161,110 +161,114 @@ const Product: NextPage = () => {
             </SwiperSlide>
           </Swiper>
 
-          <div className="flex flex-col ">
-            <h1 className="text-[2.625rem] font-header font-bold">
-              Sona Armless Chair
-            </h1>
-            <span className="font-display text-[2rem]">$400.00</span>
-            <span className="mb-[1.4375rem]">Available : In Stock</span>
+          <div className="flex flex-col items-center flex-[0.8]">
+            <div>
+              <h1 className="text-[2.625rem] font-header font-bold">
+                Sona Armless Chair
+              </h1>
+              <span className="font-display text-[2rem]">$400.00</span>
+              <span className="mb-[1.4375rem]">Available : In Stock</span>
 
-            <p className="text-[1.125rem] mb-[2.875rem] max-w-[30.125rem]">
-              Versatile elegance for dining room, office or living room. This
-              seating solution offers great appeal. Bamboo chairs combine
-              premium quality materials with unique high-style design, with the
-              advanced product engineering and packaging reinforcement, each
-              product is designed, manufactured and packaged with shipping in
-              mind.
-            </p>
+              <p className="text-[1.125rem] mb-[2.875rem] max-w-[30.125rem]">
+                Versatile elegance for dining room, office or living room. This
+                seating solution offers great appeal. Bamboo chairs combine
+                premium quality materials with unique high-style design, with
+                the advanced product engineering and packaging reinforcement,
+                each product is designed, manufactured and packaged with
+                shipping in mind.
+              </p>
 
-            {/* Pickers */}
-            <div className="flex gap-[10.3125rem]">
-              <div className="select-color">
-                <span className="uppercase font-bold text-secondary">
-                  Color
+              {/* Pickers */}
+              <div className="flex gap-[10.3125rem]">
+                <div className="select-color">
+                  <span className="uppercase font-bold text-secondary">
+                    Color
+                  </span>
+                  <ul className="flex gap-[.375rem]">
+                    <li className="border-2 border-black rounded-full p-[0.125rem] cursor-pointer">
+                      <div className="w-[1.25rem] h-[1.25rem] rounded-full bg-amber-900"></div>
+                    </li>
+                    <li className="border-2 border-transparent rounded-full p-[0.125rem] cursor-pointer">
+                      <div className="w-[1.25rem] h-[1.25rem] rounded-full bg-[#404040]"></div>
+                    </li>
+                    <li className="border-2 border-transparent rounded-full p-[0.125rem] cursor-pointer">
+                      <div className="w-[1.25rem] h-[1.25rem] rounded-full bg-[#C4C4C4]"></div>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="select-size">
+                  <span className="uppercase font-bold text-secondary">
+                    Size
+                  </span>
+                  <ul className="flex gap-[0.9375rem]">
+                    <li className="w-[24px] h-[24px] rounded-sm bg-[#E8E8E8] hover:bg-[#c7c7c7] flex justify-center items-center text-[0.625rem] cursor-pointer">
+                      XL
+                    </li>
+                    <li className="w-[24px] h-[24px] rounded-sm bg-primary hover:bg-[#c7c7c7] flex justify-center items-center text-[0.625rem] cursor-pointer">
+                      S
+                    </li>
+                    <li className="w-[24px] h-[24px] rounded-sm bg-[#E8E8E8] hover:bg-[#c7c7c7] flex justify-center items-center text-[0.625rem] cursor-pointer">
+                      M
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="flex items-center mt-[7.5rem]">
+                <span className="uppercase font-bold text-2xl mr-[1.25rem]">
+                  QTY
                 </span>
-                <ul className="flex gap-[.375rem]">
-                  <li className="border-2 border-black rounded-full p-[0.125rem] cursor-pointer">
-                    <div className="w-[1.25rem] h-[1.25rem] rounded-full bg-amber-900"></div>
-                  </li>
-                  <li className="border-2 border-transparent rounded-full p-[0.125rem] cursor-pointer">
-                    <div className="w-[1.25rem] h-[1.25rem] rounded-full bg-[#404040]"></div>
-                  </li>
-                  <li className="border-2 border-transparent rounded-full p-[0.125rem] cursor-pointer">
-                    <div className="w-[1.25rem] h-[1.25rem] rounded-full bg-[#C4C4C4]"></div>
-                  </li>
-                </ul>
-              </div>
+                <div className="flex items-center gap-1 text-[1.125rem] rounded-[1.875rem] border-2 py-[0.75rem] w-[6rem] mr-[1.5rem] justify-center">
+                  <button
+                    onClick={(e) =>
+                      quantity > 1 && quantityHandler(e.currentTarget.id)
+                    }
+                    id="subtract">
+                    -
+                  </button>
+                  <span>{String(quantity).padStart(2, "0")}</span>
+                  <button
+                    onClick={(e) =>
+                      quantity < 99 && quantityHandler(e.currentTarget.id)
+                    }
+                    id="add">
+                    +
+                  </button>
+                </div>
 
-              <div className="select-size">
-                <span className="uppercase font-bold text-secondary">Size</span>
-                <ul className="flex gap-[0.9375rem]">
-                  <li className="w-[24px] h-[24px] rounded-sm bg-[#E8E8E8] hover:bg-[#c7c7c7] flex justify-center items-center text-[0.625rem] cursor-pointer">
-                    XL
-                  </li>
-                  <li className="w-[24px] h-[24px] rounded-sm bg-primary hover:bg-[#c7c7c7] flex justify-center items-center text-[0.625rem] cursor-pointer">
-                    S
-                  </li>
-                  <li className="w-[24px] h-[24px] rounded-sm bg-[#E8E8E8] hover:bg-[#c7c7c7] flex justify-center items-center text-[0.625rem] cursor-pointer">
-                    M
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="flex items-center mt-[7.5rem]">
-              <span className="uppercase font-bold text-2xl mr-[1.25rem]">
-                QTY
-              </span>
-              <div className="flex items-center gap-1 text-[1.125rem] rounded-[1.875rem] border-2 py-[0.75rem] w-[6rem] mr-[1.5rem] justify-center">
-                <button
-                  onClick={(e) =>
-                    quantity > 1 && quantityHandler(e.currentTarget.id)
-                  }
-                  id="subtract">
-                  -
-                </button>
-                <span>{String(quantity).padStart(2, "0")}</span>
-                <button
-                  onClick={(e) =>
-                    quantity < 99 && quantityHandler(e.currentTarget.id)
-                  }
-                  id="add">
-                  +
+                <button className="btn btn-primary uppercas text-2xl !flex justify-center items-center font-bold gap-1 tracking-[0.08em] w-full max-w-[20rem]">
+                  <HiShoppingCart size={24} />
+                  SHOP NOW
                 </button>
               </div>
 
-              <button className="btn btn-primary uppercas text-2xl !flex justify-center items-center font-bold gap-1 tracking-[0.08em] w-full max-w-[20rem]">
-                <HiShoppingCart size={24} />
-                SHOP NOW
-              </button>
-            </div>
-
-            <div className="flex gap-5  items-center  mt-[4.375rem]">
-              <span className="font-bold text-[1.25rem]">SHARE</span>
-              <ul className="flex gap-1">
-                <li>
-                  <a href="#">
-                    <MdOutlineFacebook
-                      className="text-facebook hover:scale-[1.1] transition-all"
-                      size={24}
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <AiFillTwitterCircle
-                      className="text-twitter hover:scale-[1.1] transition-all"
-                      size={24}
-                    />
-                  </a>
-                </li>
-                <li className="bg-white rounded-full w-6 h-6 flex justify-center items-center hover:scale-[1.1] transition-all">
-                  <a href="#">
-                    <AiOutlineInstagram color="#333" size={21} />
-                  </a>
-                </li>
-              </ul>
+              <div className="flex gap-5  items-center  mt-[4.375rem]">
+                <span className="font-bold text-[1.25rem]">SHARE</span>
+                <ul className="flex gap-1">
+                  <li>
+                    <a href="#">
+                      <MdOutlineFacebook
+                        className="text-facebook hover:scale-[1.1] transition-all"
+                        size={24}
+                      />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <AiFillTwitterCircle
+                        className="text-twitter hover:scale-[1.1] transition-all"
+                        size={24}
+                      />
+                    </a>
+                  </li>
+                  <li className="bg-white rounded-full w-6 h-6 flex justify-center items-center hover:scale-[1.1] transition-all">
+                    <a href="#">
+                      <AiOutlineInstagram color="#333" size={21} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -272,7 +276,9 @@ const Product: NextPage = () => {
 
       <div className="container mx-auto p-5">
         <div>
-          <h1 className="mb-[3.375rem] text-[3rem] font-header text-center">Related Product</h1>
+          <h1 className="mb-[3.375rem] text-[3rem] font-header text-center">
+            Related Product
+          </h1>
           <div className="mt-5">
             <Swiper
               slidesPerView={3}
